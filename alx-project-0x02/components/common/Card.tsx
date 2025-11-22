9ceee85adf41a393ecdf4d076e6a0e6feeb7154f
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-11-22 17:44:46 +0200

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,9 +1,9 @@
-import { CardProps } from "../../interfaces";
+import { type CardProps } from "@/interfaces";
 
 const Card = ({ title, content }: CardProps) => {
   return (
-    <div className="border rounded-lg shadow-md p-4 bg-white hover:shadow-lg transition">
-      <h2 className="text-xl font-bold mb-2">{title}</h2>
+    <div className="border rounded-lg shadow p-4 bg-white">
+      <h2 className="text-xl font-semibold mb-2">{title}</h2>
       <p className="text-gray-700">{content}</p>
     </div>
   );
