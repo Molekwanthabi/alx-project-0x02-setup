688a486f09912fb19b2636f518f3b1f4e179ddde
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-11-22 17:55:11 +0200

    Refactor Button component to use children prop

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,18 +1,27 @@
-import { ButtonProps } from "../../interfaces";
+import { type ButtonProps } from "@/interfaces";
 
-const Button = ({ label, size = "medium", shape = "rounded-md", onClick }: ButtonProps) => {
-  const sizeClasses = {
-    small: "px-3 py-1 text-sm",
-    medium: "px-4 py-2 text-base",
-    large: "px-6 py-3 text-lg",
-  };
+const Button = ({ size, shape, children }: ButtonProps) => {
+  // Size styles
+  const sizeClasses =
+    size === "small"
+      ? "px-3 py-1 text-sm"
+      : size === "medium"
+      ? "px-4 py-2 text-base"
+      : "px-5 py-3 text-lg"; // large
+
+  // Shape styles
+  const shapeClasses =
+    shape === "rounded-sm"
+      ? "rounded-sm"
+      : shape === "rounded-md"
+      ? "rounded-md"
+      : "rounded-full";
 
   return (
     <button
-      onClick={onClick}
-      className={`bg-blue-600 text-white ${sizeClasses[size]} ${shape} hover:bg-blue-700 transition`}
+      className={`bg-blue-600 text-white ${sizeClasses} ${shapeClasses} hover:bg-blue-700 transition`}
     >
-      {label}
+      {children}
     </button>
   );
 };
